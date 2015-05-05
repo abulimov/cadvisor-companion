5a1c069305e759c813bf51cefa08f55ff4c30a10
Alexander Bulimov <lazywolf0@gmail.com>
2015-05-05 16:07:38 +0300

    Added more process test

--- process/history_test.go
+++ process/history_test.go
@@ -9,6 +9,46 @@ import (
 	"time"
 )
 
+func prepareHistory() (*HistoryDB, error) {
+	var history HistoryDB
+	timeStamp1 := time.Now()
+	procs1, err := GetProcesses("./testroot")
+	if err != nil {
+		return nil, err
+	}
+	// create first entry
+	entry1 := make(HistoryEntry)
+	entry1["test"] = Snapshot{timeStamp1, procs1}
+	timeStamp2 := time.Now()
+
+	// create procs for second entry
+	procs2 := make(List, 0)
+	for _, p := range procs1 {
+		// we s
+		if p.Stat.Pid != 6930 {
+			procs2 = append(procs2, p)
+		}
+	}
+
+	// update Utime for one particular process
+	someP := procs1.FindProc(uint64(6930))
+	if someP != nil {
+		someP.Stat.Utime += 500
+	} else {
+		return nil, err
+	}
+	procs2 = append(procs2, *someP)
+
+	// create second entry
+	entry2 := make(HistoryEntry)
+	entry2["test"] = Snapshot{timeStamp2, procs2}
+
+	// push both entries to history
+	history.Push(entry1)
+	history.Push(entry2)
+	return &history, nil
+}
+
 func TestHistoryPush(t *testing.T) {
 	var history HistoryDB
 	timeStamp := time.Now()
@@ -37,42 +77,10 @@ func TestHistoryPush(t *testing.T) {
 }
 
 func TestHistoryLastData(t *testing.T) {
-	var history HistoryDB
-	timeStamp1 := time.Now()
-	procs1, err := GetProcesses("./testroot")
+	history, err := prepareHistory()
 	if err != nil {
-		t.Fatal("process reading fail", err)
+		t.Fatal("preparing history failed", err)
 	}
-	// create first entry
-	entry1 := make(HistoryEntry)
-	entry1["test"] = Snapshot{timeStamp1, procs1}
-	timeStamp2 := time.Now()
-
-	// create procs for second entry
-	procs2 := make(List, 0)
-	for _, p := range procs1 {
-		// we s
-		if p.Stat.Pid != 6930 {
-			procs2 = append(procs2, p)
-		}
-	}
-
-	// update Utime for one particular process
-	someP := procs1.FindProc(uint64(6930))
-	if someP != nil {
-		someP.Stat.Utime += 500
-	} else {
-		t.Fatal("failed to get proc")
-	}
-	procs2 = append(procs2, *someP)
-
-	// create second entry
-	entry2 := make(HistoryEntry)
-	entry2["test"] = Snapshot{timeStamp2, procs2}
-
-	// push both entries to history
-	history.Push(entry1)
-	history.Push(entry2)
 	snap, err := history.GetLastData("test", 1, 1)
 	if err != nil {
 		t.Fatal("getting history last data failed", err)
@@ -84,5 +92,99 @@ func TestHistoryLastData(t *testing.T) {
 	gotCPUUsage := gotP.RelativeCPUUsage
 	if expectedCPUUsage != gotCPUUsage {
 		t.Errorf("%f not equal to expected %f", gotCPUUsage, expectedCPUUsage)
+	}
+}
+
+func TestHistoryLastDataWrongConstraints(t *testing.T) {
+	history, err := prepareHistory()
+	if err != nil {
+		t.Fatal("preparing history failed", err)
+	}
+	_, err = history.GetLastData("test", 1, 100)
+	if err == nil {
+		t.Error("GetLastData with wrong offset didn't failed when expected to fail")
+	}
+
+	_, err = history.GetLastData("test", 100, 1)
+	if err == nil {
+		t.Error("GetLastData with wrong interval didn't failed when expected to fail")
+	}
+}
+
+func TestHistoryTopCPU(t *testing.T) {
+	history, err := prepareHistory()
+	if err != nil {
+		t.Fatal("preparing history failed", err)
+	}
+	snap, err := history.GetTopCPU("test", 1, 1, 1)
+	if err != nil {
+		t.Fatal("getting history TopCPU failed", err)
+	}
+
+	gotP := snap.Processes[0]
+	expectedCPUUsage := float64(100)
+	gotCPUUsage := gotP.RelativeCPUUsage
+	if expectedCPUUsage != gotCPUUsage {
+		t.Errorf("%f not equal to expected %f", gotCPUUsage, expectedCPUUsage)
+	}
+}
+
+func TestHistoryTopCPUWrongConstraints(t *testing.T) {
+	history, err := prepareHistory()
+	if err != nil {
+		t.Fatal("preparing history failed", err)
+	}
+	_, err = history.GetTopCPU("test", 1, 1, 100)
+	if err == nil {
+		t.Error("GetTopCPU with wrong offset didn't failed when expected to fail")
+	}
+
+	_, err = history.GetTopCPU("test", 1, 100, 1)
+	if err == nil {
+		t.Error("GetTopCPU with wrong interval didn't failed when expected to fail")
+	}
+
+	_, err = history.GetTopCPU("test", 100, 1, 1)
+	if err != nil {
+		t.Error("GetTopCPU with wrong limit failed when should not")
+	}
+}
+
+func TestHistoryTopMem(t *testing.T) {
+	history, err := prepareHistory()
+	if err != nil {
+		t.Fatal("preparing history failed", err)
+	}
+	snap, err := history.GetTopMem("test", 1, 1, 1)
+	if err != nil {
+		t.Fatal("getting history TopMem failed", err)
+	}
+
+	gotP := snap.Processes[0]
+	expectedVMRSS := uint64(53984)
+	gotVMRSS := gotP.Status.VmRSS
+	if expectedVMRSS != gotVMRSS {
+		t.Errorf("%d not equal to expected %d", gotVMRSS, expectedVMRSS)
+	}
+}
+
+func TestHistoryTopMemWrongConstraints(t *testing.T) {
+	history, err := prepareHistory()
+	if err != nil {
+		t.Fatal("preparing history failed", err)
+	}
+	_, err = history.GetTopMem("test", 1, 1, 100)
+	if err == nil {
+		t.Error("GetTopMem with wrong offset didn't failed when expected to fail")
+	}
+
+	_, err = history.GetTopMem("test", 1, 100, 1)
+	if err == nil {
+		t.Error("GetTopMem with wrong interval didn't failed when expected to fail")
+	}
+
+	_, err = history.GetTopMem("test", 100, 1, 1)
+	if err != nil {
+		t.Error("GetTopMem with wrong limit failed when should not")
 	}
 }
